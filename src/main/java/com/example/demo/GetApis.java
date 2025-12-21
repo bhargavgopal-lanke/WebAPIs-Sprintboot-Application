30a5e8c8216906a4806f1198ec40fb211b765279
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-12-21 14:23:37 +0530

    created get method api's beginner api's.

--- src/main/java/com/example/demo/GetApis.java
+++ src/main/java/com/example/demo/GetApis.java
@@ -1,12 +1,14 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GetApis {
 	
+	// get values from the api which are there after ? in the form of key value pairs
 	@GetMapping("watch")
 	public String getVedioDetails( @RequestParam String v,@RequestParam String channel) {
 		return v +" "+ channel;
@@ -19,8 +21,26 @@ public class GetApis {
 	
 	// assignment
 	
-	@GetMapping("s")
+	@GetMapping("k")
 	public String assignmentApiTaskOne(@RequestParam String k, @RequestParam String i,@RequestParam String crid) {
 		return k + " " + i + " " + crid;
-	}
+	};
+	
+	// get  values from query parameters which are not assigned to any variable
+	
+	@GetMapping("c/{courseDetails}") 
+	public String getCourseDetails(@PathVariable String courseDetails) {
+		return "get course details for: " + courseDetails;
+	};
+	
+	@GetMapping("p/{postid}")
+	public String getpostDetails(@PathVariable String postid) {
+		return "get post deatils for:" + postid; 
+	};
+	
+	@GetMapping("{seoTitle}/dp/{productId}")
+	public String getAmazonProductDetails(@PathVariable String seoTitle, @PathVariable String productId) {
+		return "get amazon product details for: " + seoTitle + " with product id " + productId;
+	};
+	
 }
