893565656f8489d1a555f171590215d8f002ec84
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-12-22 16:03:13 +0530

    getter and setter functions created for post api

--- src/main/java/com/example/demo/PostApis.java
+++ src/main/java/com/example/demo/PostApis.java
@@ -1,11 +1,18 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PostApis {
+
+    private final Authentication authentication;
+
+    PostApis(Authentication authentication) {
+        this.authentication = authentication;
+    }
 	
 	@PostMapping("/auth/login")
 		public String Login(@RequestParam String email, @RequestParam String password) {
@@ -13,12 +20,16 @@ public class PostApis {
 		};
 		
 	@PostMapping("auth/createAccount")
-		public String Signup(){
-			return "Account created successfully";
+		public String Signup(@RequestBody UserData userData){
+			return "Account created successfully " + userData.getName()
+			+ ", " + userData.getEmail() + ", " + userData.getPassword() 
+			+ ", " + userData.getMobile();
 		};
 		
 	@PostMapping("auth/forgotpassword")
 		public String forgotPassword() {
 			return "password reset successfull";
 		};
+		
+		
 }
---
+++ src/main/java/com/example/demo/UserData.java
@@ -0,0 +1,44 @@
+package com.example.demo;
+
+
+public class UserData {
+	String username;
+	String email;
+	String password;
+	String mobile;
+	
+	
+	public void setname(String name) {
+		this.username = name;
+	}
+	
+	public String getName() {
+		return this.username;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getEmail() {
+		return this.email;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getPassword() {
+		return this.password;
+	}
+	
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	
+	public String getMobile() {
+		return this.mobile;
+	}
+	
+}
+
