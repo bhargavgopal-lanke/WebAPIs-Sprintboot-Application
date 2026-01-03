17d80dc73bcdbeec010b28c1f420c3d9e7ef66bc
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2026-01-03 19:00:11 +0530

    added new POST APIs for user data and example endpoints; included Lombok dependency

--- src/main/java/com/example/demo/PostApis.java
+++ src/main/java/com/example/demo/PostApis.java
@@ -1,9 +1,13 @@
 package com.example.demo;
 
+import java.security.PublicKey;
+import java.util.concurrent.Flow.Publisher;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class PostApis {
@@ -12,6 +16,21 @@ public class PostApis {
 		public String Login(@RequestParam String email, @RequestParam String password) {
 			return "login scuccessful: " + email + " " + password;
 		};
+		
+		@PostMapping("/ting/ting")
+		public String newApi(@RequestParam String ding, @RequestParam String dong) {
+			return "ting ting api is called " + ding + " " + dong;
+		}
+			
+		public static class user {
+			public String name;
+			public int age;
+		}
+		
+		@PostMapping("/list/users")
+		public String listUsers(@RequestBody user user) {
+			return "List of users" + " " + user.name + " " + user.age;
+		}
 		
 	@PostMapping("auth/createAccount")
 		public String Signup(@RequestBody UserData userData){
@@ -24,6 +43,12 @@ public class PostApis {
 		public String forgotPassword() {
 			return "password reset successfull";
 		};
+		
+		@PostMapping("user/data")
+		public String userData(@RequestBody UserDataNew userDataNew) {
+			return "user api data" + " " + userDataNew.getUsername() + " " + userDataNew.getEmail()
+			+ " " + userDataNew.getPassword() + " " + userDataNew.getMobile();
+		}
 		
 		
 }
