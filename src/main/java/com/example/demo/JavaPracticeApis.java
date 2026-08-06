a2899e9b951107ee6714347509607fb244702f4d
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2026-08-06 22:21:06 +0530

    Remove duplicate /watch endpoint from JavaPracticeApis

--- src/main/java/com/example/demo/JavaPracticeApis.java
+++ src/main/java/com/example/demo/JavaPracticeApis.java
@@ -1,15 +1,13 @@
 package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class JavaPracticeApis {
-	public static void main(String[] args) {
-		System.out.println("Hello World 4");
-	}
 	
-	@GetMapping("/watch")
-	public String getVedioaDetails() {
-		return "vedio details";
+	@GetMapping("/watchpractice")
+	public String getVideoDetails(@RequestParam String v) {
+		return v;
 	}
 }
