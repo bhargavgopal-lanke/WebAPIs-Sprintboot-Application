5d65d21f8a3a18751a950c369981913306851a86
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2026-08-07 21:07:38 +0530

    Add user info and authentication APIs with login and signup functionality

--- src/main/java/com/example/demo/JavaPracticeApis.java
+++ src/main/java/com/example/demo/JavaPracticeApis.java
@@ -1,5 +1,8 @@
 package com.example.demo;
+import java.time.chrono.IsoChronology;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +13,15 @@ public class JavaPracticeApis {
 	public String getVideoDetails(@RequestParam String v, @RequestParam String t) {
 		return v + " " + t;
 	}
+	
+	@GetMapping("api/v1/webuserinfo")
+	public String getUserInfo(@RequestParam String username, @RequestParam String criData) {
+		return "This is username"  + " " + username + " " + criData;
+	}
+	
+	@GetMapping("course/{coursepIdDetails}")
+	public String getCoursesId(@PathVariable String coursepIdDetails) {
+		return "this is the coursepid id :" + " " + coursepIdDetails;
+	}
+	
 }
